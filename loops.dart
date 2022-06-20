a7d84ddc10d8a60c28def2fae9a3f012f25046ab
GhulamQadir <hafizghulamqadir.2005@gmail.com>
2022-06-20 13:04:10 -0700

    loops practise

--- loops.dart
+++ loops.dart
@@ -6,14 +6,43 @@ void main() {
   //   print(i);
   // }
 
-  var proLanguages = ["JavaScript", "Dart", "Python", "Java", "C++"];
-  for (var i = 0; i < proLanguages.length; i++) {
-    // print(proLanguages[i]);
-    if (proLanguages[i] == "Dart") {
-      print(proLanguages[i]);
-      break;
-    }
-  }
+  // var proLanguages = ["JavaScript", "Dart", "Python", "Java", "C++"];
+  // for (var i = 0; i < proLanguages.length; i++) {
+  //   // print(proLanguages[i]);
+  //   if (proLanguages[i] == "Dart") {
+  //     print(proLanguages[i]);
+  //     break;
+  //   }
+  // }
 
+//
+//
+//
   // infinite loop = never stop on specific count
+  // while loop
+  // var count = 0;
+  // while (true) {
+  //   print("Hacking started $count...");
+  //   count++;
+  //   if (count == 10000) {
+  //     print("Hacked Successfully 😀");
+  //     break;
+  //   }
+  // }
+
+  // bool isOk = true;
+  // bool somethingWrong = false;
+  // bool conditions = isOk || somethingWrong;
+  // while (conditions) {
+  //   print("Chal rha ha 😀");
+  // }
+
+  //
+  //
+  // do while loop(first run then check the condition no matter condition is true or false)
+  int count = 404;
+  do {
+    print("Error: $count");
+    count++;
+  } while (count <= 401);
 }
